bc8cf7e2b81535d497d171f7153187f12618315f
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2022-03-24 15:15:44 +0900

    Feat convert to json

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Newtonsoft.Json;
+
+namespace Company
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public Employee(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee("user_id", "name");
+
+            string json = JsonConvert.SerializeObject(employee, Formatting.Indented);
+
+            Console.WriteLine(json);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# CSON
+
+```bash
+dotnet add package Newtonsoft.Json --version 13.0.1
+```
